b90d6a77b46baa9eb2c7bf255ef7c723e64ae3b0
Rodrigo Souza <rsouza01@gmail.com>
2018-08-02 16:25:24 -0300

    Update TaskListRoutes.js

--- src/API/api/routes/TaskListRoutes.js
+++ src/API/api/routes/TaskListRoutes.js
@@ -4,11 +4,11 @@ module.exports = function(app) {
 	var taskList = require('../controllers/TaskListController');
 
 	//TaskList
-	app.route('/tasks')
+	app.route('/api/tasks')
 		.get(taskList.list_all_tasks)
 		.post(taskList.create_task);
 
-	app.route('/tasks/:taskId')
+	app.route('/api/tasks/:taskId')
 		.get(taskList.read_task)
 		.put(taskList.update_task)
 		.delete(taskList.delete_task);
