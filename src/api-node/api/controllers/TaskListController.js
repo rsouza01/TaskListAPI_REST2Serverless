85ebff56d35650f70eb1f774bfd241009dc729ed
Rodrigo Souza <rsouza01@gmail.com>
2018-08-03 16:28:20 -0300

    SQL query added

--- src/api-node/api/controllers/TaskListController.js
+++ src/api-node/api/controllers/TaskListController.js
@@ -1,5 +1,27 @@
 'use strict';
 
+function execSQLQuery(sqlQry, res){
+	const connection = mysql.createConnection({
+		host     : 'db4free.net',
+		port     :  3306,
+		user     : 'user_task_list',
+		password : 'password_task_list',
+		database : 'db_task_list'
+	});
+
+	connection.query(sqlQry, function(error, results, fields){
+		if(error) 
+			res.json(error);
+		else
+			res.json(results);
+		connection.end();
+		console.log('[*] Query executed with success.');
+	});
+}
+
+
+
+
 exports.list_all_tasks = function(req, res) {
     console.log('[FC] list_all_tasks');
 
